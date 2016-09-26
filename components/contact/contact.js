5d4bbce8da4c6d7c676326099887ae3f8364015d
simmch <Chris@Chriss-iMac.local>
2016-09-25 22:25:24 -0400

    The only thing left to add is php for contact form

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -5,7 +5,14 @@
 	angular
 		.module('contact.ctrl', ['ngMaterial', 'ui.router'])
 		.controller('contactController', function($scope, $state, $mdSidenav, $http){
-			var vm = this;
+
+			$scope.facebook = 'http://facebook.com';
+			$scope.instagram = 'http://instagram.com';
+
+			$scope.linkModelFunc = function (url){
+							  console.log('link model function');
+							  $window.open(url);
+							}
 		});
 
 })();
